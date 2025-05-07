7edd0776616a9793c62793d21ee693fd5cfbf219
Angel Puch <angeljonathanpuchhernandez@gmail.com>
2025-05-07 09:45:23 -0600

    Agrega BebidaDAO y Bebida.java, modifica ClienteDAO

---
+++ src/javafxexpendio/modelo/pojo/Bebida.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxexpendio.modelo.pojo;
+
+/**
+ *
+ * @author Dell
+ */
+public class Bebida {
+    private int idBebida;
+    private String bebida;
+    private int stock;
+    private int stockMinimo;
+    private double precio;
+
+    public Bebida() {
+    }
+
+    public Bebida(int idBebida, String bebida, int stock, int stockMinimo, double precio) {
+        this.idBebida = idBebida;
+        this.bebida = bebida;
+        this.stock = stock;
+        this.stockMinimo = stockMinimo;
+        this.precio = precio;
+    }
+
+    public int getIdBebida() {
+        return idBebida;
+    }
+
+    public void setIdBebida(int idBebida) {
+        this.idBebida = idBebida;
+    }
+
+    public String getBebida() {
+        return bebida;
+    }
+
+    public void setBebida(String bebida) {
+        this.bebida = bebida;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
